fb4b9db60b92fc0b4d3f220d5412b87b06b12962
Ishan Khare <ishankhare07@gmail.com>
2015-06-30 00:07:30 +0530

    moved from breadth first to depth first approach also added support from maximum depth and breadth of tree

--- main.py
+++ main.py
@@ -3,32 +3,50 @@ from bs4 import BeautifulSoup
 from saver import Saver
 
 class Scrapper:
-    def __init__(self, start_url, savefile):
+    def __init__(self, start_url, savefile, max_depth=10, max_width=100):
+        """
+            max_depth: maximum recursion depth to follow for each link
+            max_width: maximum number of dict keys, i.e. width of tree
+        """
         self.parser = Parser()
         self.start_url = start_url
-        self.saver = Saver(savefile)
+        self.saver = Saver(savefile, max_width)
+        self.max_depth = max_depth
+        self.saver.starting_url(self.start_url)
 
-    def start_scrapping(self, url=None):
-        if url == None:
-            url = self.start_url
-        nested_urls = self.step_down(url)
-        if not url == self.start_url:
+    def start_scrapping(self, depth=0, start_url = None):
+        if depth == self.max_depth:
             return
-        elif nested_urls == None:
+        if start_url == None:
+            start_url = self.start_url
+        nested_urls = self.get_urls(start_url)
+        bool_break = self.save_data(start_url, nested_urls)
+        if not bool_break:
+            exit()
+
+        if nested_urls == None:
             return
         else:
             for url in nested_urls:
-                self.step_down(url)
+                self.start_scrapping(depth+1, url)
 
-    def step_down(self, root_url):
+    def get_urls(self, url):
         try:
-            response = requests.get(root_url)
+            response = requests.get(url)
             web_page = response.content
             urls = self.parser.get_links(web_page)
-            self.saver.save(root_url, urls)
-        except Exception:
-            return None
+        except requests.exceptions.RequestException as re:
+            print(re)
+            return
         return urls
+
+    def save_data(self, start_url, nested_urls):
+        reply = self.saver.save(start_url, nested_urls)
+        if not reply:
+            return False
+        else:
+            return True
+
 
 class Parser:
     def get_links(self, html_doc):
--- saver.py
+++ saver.py
@@ -1,11 +1,17 @@
 import shelve
 
 class Saver:
-    def __init__(self, savefile):
+    def __init__(self, savefile, max_width):
+        self.max_width = max_width
         self.file = shelve.open(savefile)
 
     def starting_url(self, url):
-        self.data["starting url"] = url
+        self.file["starting url"] = url
 
     def save(self, url, url_list):
+        num_keys = len(list(self.file.keys()))
+        if num_keys == self.max_width:
+            return False
         self.file[url] = url_list
+        print("total keys=>", num_keys)
+        return True
